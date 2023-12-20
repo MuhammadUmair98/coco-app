35c60a55f031c254e13da74270353399592dd6f6
MubashirMaqbool <mubashir.maqbool@vasol.net>
2023-12-20 18:09:12 +0500

    task status added

--- front-end/src/components/company/subTask/addSubTask.vue
+++ front-end/src/components/company/subTask/addSubTask.vue
@@ -20,13 +20,13 @@
                         <div>
                             <label for="taskCompletion"
                                 class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-                                SubTask Completion
+                                SubTask Status
                             </label>
                             <select name="taskCompletion" id="taskCompletion" v-model="subTask.subtask_completion"
                                 class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                 required>
-                                <option value="0">0</option>
-                                <option value="1">1</option>
+                                <option value="0">Completed</option>
+                                <option value="1">In Complete</option>
                             </select>
                         </div>
                         <div>

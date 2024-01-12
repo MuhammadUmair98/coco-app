99b6e9d59b64dd87139698aa69c0c7241a4ab2a5
MuhammadUmaira98 <MuhammadUmaira98@gmail.com>
2024-01-13 00:43:16 +0500

    increased the z-index of toast vue js

--- front-end/src/components/toastComponent.vue
+++ front-end/src/components/toastComponent.vue
@@ -1,40 +1,42 @@
 <!-- src/components/Toast.vue -->
 <template>
+  <div
+    style="z-index: 1000"
+    class="fixed bottom-4 right-4 flex flex-col space-y-4"
+    ref="toastContainer"
+  >
     <div
-      class="fixed bottom-4 right-4 flex flex-col space-y-4"
-      ref="toastContainer"
+      v-for="(toast, index) in toasts"
+      :key="index"
+      :class="[
+        'rounded-lg p-4 shadow-lg',
+        'transition-all duration-500',
+        toast.type === 'success'
+          ? 'bg-green-800 text-white'
+          : 'bg-red-800 text-white',
+      ]"
     >
-      <div
-        v-for="(toast, index) in toasts"
-        :key="index"
-        :class="[
-          'rounded-lg p-4 shadow-lg',
-          'transition-all duration-500',
-          toast.type === 'success' ? 'bg-green-800 text-white' : 'bg-red-800 text-white',
-        ]"
-      >
-        {{ toast.message }}
-      </div>
+      {{ toast.message }}
     </div>
-  </template>
-  
-  <script>
-  export default {
-    data() {
-      return {
-        toasts: [],
-      };
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      toasts: [],
+    };
+  },
+  methods: {
+    showToast(message, type = "success", duration = 5000) {
+      const toast = { message, type };
+      this.toasts.push(toast);
+
+      setTimeout(() => {
+        this.toasts.shift();
+      }, duration);
     },
-    methods: {
-      showToast(message, type = 'success', duration = 5000) {
-        const toast = { message, type };
-        this.toasts.push(toast);
-  
-        setTimeout(() => {
-          this.toasts.shift();
-        }, duration);
-      },
-    },
-  };
-  </script>
-  
+  },
+};
+</script>
